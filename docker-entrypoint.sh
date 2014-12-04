567919c4d761d6ed6fd72804f32b7f2ba0b59e23
Nicholas Katsaros <nick@nickkatsaros.com>
2014-12-03 23:44:27 -0500

    switched back to gosu now that it works for bamboo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'bamboo' ]; then
   chown -R atlassian "$BAMBOO_HOME"
   umask 0027
-  exec su atlassian -s /bin/bash -c '/opt/atlassian/bin/start-bamboo.sh -fg'
+  exec gosu atlassian /opt/atlassian/bin/start-bamboo.sh -fg
 fi
 
 exec "$@"
